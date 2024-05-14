245850c92c8c0ee1d1ca72a2fb419b5258c9fdb4
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-14 18:40:21 +0430

    stock images

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
   const [loading, setLoading] = useState(false)
-  const [photos, setPhotos] = useState(data)
+  const [photos, setPhotos] = useState([])
   const fetchImages = async () => {
     let url;
     url = `${mainUrl}? clientID=_NKtnwsrP0WzmXDdw_kQaplcDpXxad0YhAF097x0wwQ`;
