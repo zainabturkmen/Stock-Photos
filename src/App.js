af14387768f3666346d146666454fc3ff4e6cb37
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-15 16:11:21 +0430

    stock images

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     const urlQuery = `&query=${query}`
 
       if (query) {
-        url = `${searchUrl}${clientID}`
+        url = `${searchUrl}${clientID}${urlPage}`
       }
 
 
