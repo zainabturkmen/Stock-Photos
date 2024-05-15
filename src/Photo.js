8856e83df89748afb8b0a03ac245dca13a814603
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-15 10:30:06 +0430

    stock images

--- src/Photo.js
+++ src/Photo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Photo = ({urls:{regular}, alt_description}) => {
   return <article className='photo'>
-<img src={regular} />
+<img src={regular}  alt={alt_description}/>
   </article>
 }
 
