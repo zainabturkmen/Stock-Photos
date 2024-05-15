e75ce4884bc4da7a97a61dadcb8b2dfdbb2a4d9c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-15 09:02:31 +0430

    stock images

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 
-const clientID = `?client_id=${}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
