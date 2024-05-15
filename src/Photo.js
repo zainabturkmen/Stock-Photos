1e665e97215c181176174f81731cfdb8c475387f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-15 10:35:19 +0430

    stock images

--- src/Photo.js
+++ src/Photo.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const Photo = ({urls:{regular}, alt_description}) => {
-  return <article className='photo'>
-<img src={regular}  alt={alt_description}/>
-  </article>
-}
+const Photo = ({
+  urls: { regular },
+  alt_description,
+  likes,
+  user: {
+    name,
+    portfolio_url,
+    profile_image: { medium },
+  },
+}) => {
+  return (
+    <article className="photo">
+      <img src={regular} alt={alt_description} />
+      <div className="photo-info">
+        <div>
+          <h4>{name}</h4>
+          <p>{likes} likes</p>
+        </div>
+        <a href={portfolio_url}>
+          
+        </a>
+
+      </div>
+    </article>
+  );
+};
 
 export default Photo;
