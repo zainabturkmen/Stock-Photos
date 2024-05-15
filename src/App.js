528e994546e4f3fc5583b4f69f3f9411232796f5
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-15 16:36:02 +0430

    stock images

--- src/App.js
+++ src/App.js
@@ -29,6 +29,9 @@ function App() {
       const data = await response.json();
 
       setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          
+        }
         if (query) {
           return [...oldPhotos, ...data.results];
         } else {
